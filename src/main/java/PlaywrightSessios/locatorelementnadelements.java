eabd01b7373a206448f2a9570ad61e2943c1fcce
Gowri Prabhu <gowriprabhu.sp@gmail.com>
2025-12-23 19:25:28 +0530

    initial commit

---
+++ src/main/java/PlaywrightSessios/locatorelementnadelements.java
@@ -0,0 +1,37 @@
+package PlaywrightSessios;
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.*;
+
+import java.lang.reflect.Method;
+import java.util.List;
+public class locatorelementnadelements {
+
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        BrowserContext browserContext=browser.newContext();
+        Page page=browserContext.newPage();
+        page.navigate("https://www.orangehrm.com/en/30-day-free-trial");
+        Locator countryoptions = page.locator("//select/option");
+        int count=page.locator("//select/option").count();
+        System.out.println("Total number of dropdown options are: "+count);
+
+        //Method 1:
+        // for(int i=0;i<count;i++){
+        //     String optiontext=page.locator("//select/option").nth(i).textContent();
+        //     System.out.println(optiontext);
+        // }
+
+
+        //Method 2:
+           List<String> countryOptionText = countryoptions.allTextContents();
+           for(String text : countryOptionText){
+               System.out.println(text);
+           }
+
+
+
+    }
+}
