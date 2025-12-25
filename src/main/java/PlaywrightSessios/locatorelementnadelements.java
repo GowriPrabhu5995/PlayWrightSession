c8cfc9f06256b1a6de288a8977ddc8e3b892be09
Gowri Prabhu <gowriprabhu.sp@gmail.com>
2025-12-25 22:59:20 +0530

    Locators

--- src/main/java/PlaywrightSessios/locatorelementnadelements.java
+++ src/main/java/PlaywrightSessios/locatorelementnadelements.java
@@ -1,25 +1,29 @@
 package PlaywrightSessios;
 import com.microsoft.playwright.*;
+import java.lang.*;
 
-import java.lang.reflect.Method;
+//git import java.lang.reflect.Method;
 import java.util.List;
-public class locatorelementnadelements {
+public class locatorelementnadelements  {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         BrowserContext browserContext=browser.newContext();
         Page page=browserContext.newPage();
         page.navigate("https://www.orangehrm.com/en/30-day-free-trial");
+       // Thread.sleep(3000);
+      Locator contactSales = page.locator("text=Contact Sales").last();
+        contactSales.click();
         Locator countryoptions = page.locator("//select/option");
         int count=page.locator("//select/option").count();
         System.out.println("Total number of dropdown options are: "+count);
 
         //Method 1:
-        // for(int i=0;i<count;i++){
-        //     String optiontext=page.locator("//select/option").nth(i).textContent();
-        //     System.out.println(optiontext);
-        // }
+        for(int i=0;i<count;i++){
+            String optiontext=page.locator("//select/option").nth(i).textContent();
+            System.out.println(optiontext);
+        }
 
 
         //Method 2:git commit
@@ -29,7 +33,9 @@ public class locatorelementnadelements {
                System.out.println(text);
            }
 
-
+browserContext.close();
+browser.close();
+playwright.close();
 
     }
 }
